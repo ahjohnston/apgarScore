d3d6e195ba54570a4670097353757b78f249cd81
Annie Johnston <anne.johnston@slalom.com>
2022-12-16 22:54:31 -0500

    set up get records by id & date & goalId, delete record, patch record

--- api/src/main/java/com/apgarscore/controller/RecordController.java
+++ api/src/main/java/com/apgarscore/controller/RecordController.java
@@ -25,17 +25,16 @@ class RecordController {
     private RecordRepository recordRepository;
 
     @CrossOrigin
-    @PostMapping(path = "/records/add") 
+    @PostMapping(path = "/records/add")
     public Record addRecord(
             @RequestParam Integer goalID,
-            @RequestParam String plan,
+            @RequestParam(required = false) String plan,
             @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate,
 
-            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate,
             @RequestParam boolean complete,
 
-            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date dateComplete
-            ) {
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date dateComplete) {
 
         Record record = new Record();
         record.setGoalID(goalID);
@@ -55,13 +54,57 @@ class RecordController {
         // "status","saved")
         // );
 
-    };
+    }
 
     @CrossOrigin
-    @GetMapping(path = "/records/view")
+    @PatchMapping(path = "/records/byId")
+    public ResponseEntity<Record> markComplete(
+            @RequestParam Integer id,
+            @RequestParam(required = false) boolean complete,
+            @RequestParam(required = false) String plan) {
+        Record updated = recordRepository.findById(id).get();
+
+        if (complete)
+            updated.setComplete(complete);
+        if (plan != null)
+            updated.setPlan(plan);
+        recordRepository.save(updated);
+
+        return ResponseEntity.ok(updated);
+    }
+
+    @CrossOrigin
+    @GetMapping(path = "/records/all")
     public @ResponseBody Iterable<Record> getAllRecords() {
         // This returns a JSON or XML with the goals
         return recordRepository.findAll();
         // return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @CrossOrigin
+    @GetMapping(path = "/records/byDate")
+    public ResponseEntity<List<Record>> getRecordsByStartDate(
+            @RequestParam @DateTimeFormat(pattern = "dd-MM-yyyy") Date startDate) {
+        return new ResponseEntity<List<Record>>(recordRepository.findByStartDate(startDate), HttpStatus.OK);
+    }
+
+    @CrossOrigin
+    @GetMapping(path = "/records/byId")
+    public Optional<Record> getRecordsById(@RequestParam Integer id) {
+        return recordRepository.findById(id);
+    }
+
+    @CrossOrigin
+    @GetMapping(path = "/records/byGoalID")
+    public ResponseEntity<List<Record>> getRecordsByGoalID(@RequestParam Integer goalID) {
+        return new ResponseEntity<List<Record>>(recordRepository.findByGoalID(goalID), HttpStatus.OK);
+    }
+
+    @CrossOrigin
+    @DeleteMapping(path = "/records/byId")
+    public String deleteRecordById(@RequestParam Integer id) {
+
+        recordRepository.deleteById(id);
+        return "Record deleted"; // best practice: add a confirmation that record was deleted
     }
 }
--- api/src/main/java/com/apgarscore/repository/RecordRepository.java
+++ api/src/main/java/com/apgarscore/repository/RecordRepository.java
@@ -1,9 +1,25 @@
 package com.apgarscore.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.apgarscore.model.Record;
 
-public interface RecordRepository extends CrudRepository<Record, Integer>{
-    
+@Repository
+public interface RecordRepository extends JpaRepository<Record, Integer> {
+    List<Record> findByStartDate(Date startDate);
+
+    List<Record> findByGoalID(Integer goalID);
+
+    Optional<Record> findById(Integer id);
+
+    // Optional<Record> setComplete(Integer id, Boolean complete);
+
+    void deleteById(Integer id); // is there a way to validate that this method has successfully executed
+
 }
