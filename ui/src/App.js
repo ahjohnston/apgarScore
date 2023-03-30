a13832c7bf29b06aa3de3825b0096ba1a8ce34cb
Annie Johnston <anne.johnston@slalom.com>
2023-03-30 11:12:08 -0700

    change name to GoalGrid

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,4 @@
 import {AddGoalModal} from './components/AddGoalModal.tsx';
-import {ViewGoals} from './components/ViewGoals.tsx';
 import { GoalGrid } from './components/GoalGrid.tsx';
 
 import './App.scss';
@@ -12,7 +11,6 @@ function App() {
             </header>
             <AddGoalModal cadence="daily"/>
             <div className='container centered-content'>
-                {/* <ViewGoals/> */}
             </div>
                 <GoalGrid cadence="daily"/>
                 <GoalGrid cadence="weekly"/>
